6952f5665d2030a9e622adb0c8cc54eb550c0d81
alshady <allen.schade@gmail.com>
2018-09-28 21:18:33 -0700

    small update

--- scenes/tools/rigid_camera.gd
+++ scenes/tools/rigid_camera.gd
@@ -7,7 +7,7 @@ func _physics_process(delta):
 		move_vector+=Vector2(10,0)
 		#apply_impulse(Vector2(0,0),Vector2(200,0))
 		#move_and_collide( Vector2(10,0) * (delta * 70), false, true, false)
-	elif Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left"):
 		#apply_impulse(Vector2(0,0),Vector2(-200,0))
 		move_vector+=Vector2(-10,0)
 		#move_and_collide( Vector2(-10,0) * (delta * 70), true, true, false)
@@ -15,10 +15,10 @@ func _physics_process(delta):
 		#apply_impulse(Vector2(0,0),Vector2(0,-200))
 		move_vector+=Vector2(0,-10)
 		#move_and_collide( Vector2(0,-10) * (delta * 70), true, true, false)
-	elif Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down"):
 		move_vector+=Vector2(0,10)
 		#apply_impulse(Vector2(0,0),Vector2(0,200))
 		#for controller in Input.get_connected_joypads():
 		#	prints("name:", Input.get_joy_name(controller),"guid:", Input.get_joy_axis(0,0), delta * 100)
 		#move_and_collide( Vector2(0,10)* (delta * 70), true, true, false)
-		move_and_collide( move_vector * (delta * 70), true, true, false)
+	move_and_collide( move_vector * (delta * 70), true, true, false)
